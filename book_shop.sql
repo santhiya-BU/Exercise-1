bd60115e8781169aec3afcb1ee8d34d724632294
unknown <santhiyasbcab@gmail.com>
2025-04-12 20:37:52 +0530

    database update

--- book_shop.sql
+++ book_shop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 25, 2025 at 07:00 PM
+-- Generation Time: Apr 12, 2025 at 05:05 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.0.13
 
@@ -30,6 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `authors` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
+  `country` varchar(50) NOT NULL,
   `bio` text DEFAULT NULL,
   `created` datetime DEFAULT current_timestamp(),
   `modified` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp()
@@ -39,10 +40,41 @@ CREATE TABLE `authors` (
 -- Dumping data for table `authors`
 --
 
-INSERT INTO `authors` (`id`, `name`, `bio`, `created`, `modified`) VALUES
-(1, 'J.K. Rowling', 'Author of the Harry Potter series', '2025-03-25 23:28:04', '2025-03-25 23:28:04'),
-(2, 'George R.R. Martin', 'Author of A Song of Ice and Fire', '2025-03-25 23:28:04', '2025-03-25 23:28:04'),
-(3, 'J.R.R. Tolkien', 'Author of The Lord of the Rings', '2025-03-25 23:28:04', '2025-03-25 23:28:04');
+INSERT INTO `authors` (`id`, `name`, `country`, `bio`, `created`, `modified`) VALUES
+(1, 'J.K. Rowling', 'USA', 'Author of the Harry Potter series', '2025-03-25 23:28:04', '2025-04-12 14:52:36'),
+(2, 'George R.R. Martin', 'USA', 'Author of A Song of Ice and Fire', '2025-03-25 23:28:04', '2025-04-11 23:30:56'),
+(3, 'J.R.R. Tolkien', 'USA', 'Author of The Lord of the Rings', '2025-03-25 23:28:04', '2025-04-11 23:31:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `authors_books`
+--
+
+CREATE TABLE `authors_books` (
+  `author_id` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `authors_publishers`
+--
+
+CREATE TABLE `authors_publishers` (
+  `author_id` int(11) NOT NULL,
+  `publisher_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `authors_publishers`
+--
+
+INSERT INTO `authors_publishers` (`author_id`, `publisher_id`) VALUES
+(1, 1),
+(1, 2),
+(2, 1);
 
 -- --------------------------------------------------------
 
@@ -56,6 +88,7 @@ CREATE TABLE `books` (
   `publisher_id` int(11) DEFAULT NULL,
   `author_id` int(11) DEFAULT NULL,
   `published_year` date DEFAULT NULL,
+  `status` varchar(50) NOT NULL,
   `created` datetime DEFAULT current_timestamp(),
   `modified` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -64,13 +97,24 @@ CREATE TABLE `books` (
 -- Dumping data for table `books`
 --
 
-INSERT INTO `books` (`id`, `title`, `publisher_id`, `author_id`, `published_year`, `created`, `modified`) VALUES
-(1, 'Harry Potter and the Sorcerer’s Stone', NULL, 1, '0000-00-00', '2025-03-25 23:28:04', '2025-03-25 23:28:04'),
-(2, 'Harry Potter and the Chamber of Secrets', NULL, 1, '0000-00-00', '2025-03-25 23:28:04', '2025-03-25 23:28:04'),
-(3, 'A Game of Thrones', NULL, 2, '0000-00-00', '2025-03-25 23:28:04', '2025-03-25 23:28:04'),
-(4, 'A Clash of Kings', NULL, 2, '0000-00-00', '2025-03-25 23:28:04', '2025-03-25 23:28:04'),
-(5, 'The Hobbit', NULL, 3, '0000-00-00', '2025-03-25 23:28:04', '2025-03-25 23:28:04'),
-(6, 'The Lord of the Rings', NULL, 3, '0000-00-00', '2025-03-25 23:28:04', '2025-03-25 23:28:04');
+INSERT INTO `books` (`id`, `title`, `publisher_id`, `author_id`, `published_year`, `status`, `created`, `modified`) VALUES
+(1, 'Harry Potter and the Sorcerer’s Stone', 1, 1, '1997-01-15', 'published', '2025-03-25 23:28:04', '2025-04-12 14:50:47'),
+(2, 'Harry Potter and the Chamber of Secrets', 1, 1, '1997-01-01', 'published', '2025-03-25 23:28:04', '2025-04-11 23:30:18'),
+(3, 'A Game of Thrones', 2, 2, '1997-01-01', 'published', '2025-03-25 23:28:04', '2025-04-12 14:51:00'),
+(4, 'A Clash of Kings', 2, 2, '1997-01-01', 'published', '2025-03-25 23:28:04', '2025-04-12 14:51:09'),
+(5, 'The Hobbit', 3, 3, '1997-01-01', 'published', '2025-03-25 23:28:04', '2025-04-12 14:51:16'),
+(6, 'The Lord of the Rings', 3, 3, '1997-01-01', 'published', '2025-03-25 23:28:04', '2025-04-12 14:51:26');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `books_publishers`
+--
+
+CREATE TABLE `books_publishers` (
+  `book_id` int(11) NOT NULL,
+  `publisher_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -92,7 +136,7 @@ CREATE TABLE `publishers` (
 --
 
 INSERT INTO `publishers` (`id`, `name`, `address`, `phone`, `created`, `modified`) VALUES
-(1, 'Bloomsbury', 'United Kingdom', '9638527410', '2025-03-25 23:29:41', '2025-03-25 23:29:41'),
+(1, 'Bloomsbury', 'United Kingdom', '9638527410', '2025-03-25 23:29:41', '2025-04-12 14:53:46'),
 (2, 'Bantam Books', 'United States', '9638527410', '2025-03-25 23:29:41', '2025-03-25 23:29:41'),
 (3, 'HarperCollins', 'United States', '9638527410', '2025-03-25 23:29:41', '2025-03-25 23:29:41'),
 (4, 'Houghton Mifflin', 'United States', '9638527410', '2025-03-25 23:29:41', '2025-03-25 23:29:41');
@@ -108,12 +152,33 @@ ALTER TABLE `authors`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `authors_books`
+--
+ALTER TABLE `authors_books`
+  ADD PRIMARY KEY (`author_id`,`book_id`),
+  ADD KEY `book_id` (`book_id`);
+
+--
+-- Indexes for table `authors_publishers`
+--
+ALTER TABLE `authors_publishers`
+  ADD PRIMARY KEY (`author_id`,`publisher_id`),
+  ADD KEY `publisher_id` (`publisher_id`);
+
+--
 -- Indexes for table `books`
 --
 ALTER TABLE `books`
   ADD PRIMARY KEY (`id`),
   ADD KEY `publisher_id` (`publisher_id`),
   ADD KEY `author_id` (`author_id`);
+
+--
+-- Indexes for table `books_publishers`
+--
+ALTER TABLE `books_publishers`
+  ADD PRIMARY KEY (`book_id`,`publisher_id`),
+  ADD KEY `publisher_id` (`publisher_id`);
 
 --
 -- Indexes for table `publishers`
@@ -135,7 +200,7 @@ ALTER TABLE `authors`
 -- AUTO_INCREMENT for table `books`
 --
 ALTER TABLE `books`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `publishers`
@@ -148,11 +213,32 @@ ALTER TABLE `publishers`
 --
 
 --
+-- Constraints for table `authors_books`
+--
+ALTER TABLE `authors_books`
+  ADD CONSTRAINT `authors_books_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `authors` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `authors_books_ibfk_2` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `authors_publishers`
+--
+ALTER TABLE `authors_publishers`
+  ADD CONSTRAINT `authors_publishers_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `authors` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `authors_publishers_ibfk_2` FOREIGN KEY (`publisher_id`) REFERENCES `publishers` (`id`) ON DELETE CASCADE;
+
+--
 -- Constraints for table `books`
 --
 ALTER TABLE `books`
   ADD CONSTRAINT `books_ibfk_1` FOREIGN KEY (`publisher_id`) REFERENCES `publishers` (`id`) ON DELETE SET NULL,
   ADD CONSTRAINT `books_ibfk_2` FOREIGN KEY (`author_id`) REFERENCES `authors` (`id`) ON DELETE SET NULL;
+
+--
+-- Constraints for table `books_publishers`
+--
+ALTER TABLE `books_publishers`
+  ADD CONSTRAINT `books_publishers_ibfk_1` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `books_publishers_ibfk_2` FOREIGN KEY (`publisher_id`) REFERENCES `publishers` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
